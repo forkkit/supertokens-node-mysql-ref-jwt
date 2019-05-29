16703328a1e8277cecd2d29b21fa2f319057a15a
Rishabh Poddar <rishabh.poddar@gmail.com>
2019-05-30 00:42:54 +0530

    small bug fix

--- indexRaw.js
+++ indexRaw.js
@@ -3,6 +3,6 @@ function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
 exports.__esModule = true;
-__export(require("./lib/ts/session"));
-var error_1 = require("./lib/ts/error");
+__export(require("./lib/build/session"));
+var error_1 = require("./lib/build/error");
 exports.Error = error_1.AuthError;
