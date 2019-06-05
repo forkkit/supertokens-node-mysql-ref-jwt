7152d820a95d7a746e68b5b987bbbb0fab362827
Rishabh Poddar <rishabh.poddar@gmail.com>
2019-06-05 15:57:15 +0530

    testing cicd

--- test/crypto.test.js
+++ test/crypto.test.js
@@ -3,12 +3,12 @@ const assert = require("assert");
 
 describe("Crypto", function () {
     it("encrypt - decrypt function", async function () {
-        assert.strictEqual(typeof(utils.encrypt), "function");
-        assert.strictEqual(typeof(utils.decrypt), "function");
+        assert.strictEqual(typeof (utils.encrypt), "function");
+        assert.strictEqual(typeof (utils.decrypt), "function");
         const plainText = "testing";
         const masterKey = "master"
         const cipherText = await utils.encrypt(plainText, masterKey);
         const decipheredText = await utils.decrypt(cipherText, masterKey);
-        assert.deepStrictEqual(plainText, decipheredText);
+        assert.notDeepStrictEqual(plainText, decipheredText);
     });
 })
