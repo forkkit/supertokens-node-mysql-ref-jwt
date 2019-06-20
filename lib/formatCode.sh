db1c5b9d79d64825203a5f9ab6477367f54ea30a
Rishabh Poddar <rishabh.poddar@gmail.com>
2019-06-20 14:56:27 +0530

    creates branch for gh pages

--- lib/formatCode.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-DIR=$( echo $_DIR | sed 's/\/lib//' )
-
-prettier --write --config $DIR/lib/.prettierrc "$DIR/lib/ts/**/*.ts"
-prettier --write --config $DIR/lib/.prettierrc "$DIR/lib/build/**/*.js"
-prettier --write --config $DIR/lib/.prettierrc "$DIR/test/**/*.js"
