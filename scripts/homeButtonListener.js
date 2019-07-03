759b816b7e2ed85e7526ed98fde5219487d5dfc5
Rishabh Poddar <rishabh.poddar@gmail.com>
2019-07-03 14:37:25 +0000

    [skip ci]
    Deploy website version based on 606d862c47bf96fecda7819f4326951a435bc270

--- scripts/homeButtonListener.js
+++ scripts/homeButtonListener.js
@@ -1,4 +1,63 @@
 document.addEventListener("DOMContentLoaded", () => {
+    function uncollapse(node, title, currNav) {
+        node.classList.remove("hide");
+        currNav.children[0].innerHTML = title + '<span class="arrow rotate"><svg width="24" height="24" viewBox="0 0 24 24"><path fill="#565656" d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6-6 6z"></path><path d="M0 0h24v24H0z" fill="none"></path></svg></span>';
+    }
+
+    function collapse(node, title, currNav) {
+        node.classList.add("hide");
+        currNav.children[0].innerHTML = title + '<span class="arrow"><svg width="24" height="24" viewBox="0 0 24 24"><path fill="#565656" d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6-6 6z"></path><path d="M0 0h24v24H0z" fill="none"></path></svg></span>'
+    }
+
+    function isDescendant(parent, child) {
+        var node = child.parentNode;
+        while (node != null) {
+            if (node == parent) {
+                return true;
+            }
+            node = node.parentNode;
+        }
+        return false;
+    }
+
+    let navGroupElements = document.getElementsByClassName("navGroup subNavGroup");
+
+    let activeItem = document.getElementsByClassName("navListItemActive")[0];
+
+    for (let i = 0; i < navGroupElements.length; i++) {
+        let currNav = navGroupElements[i];
+        const title = currNav.children[0].innerText;
+        const content = navGroupElements[i].childNodes[1];
+        currNav.children[0].classList.add("collapsible");
+
+
+        collapse(content, title, currNav);
+        currNav.childNodes[0].addEventListener("click", function () {
+            if (!content.classList.contains("hide")) {
+                collapse(content, title, currNav);
+            } else {
+                uncollapse(content, title, currNav);
+            }
+        });
+
+        if (isDescendant(currNav, activeItem)) {
+            uncollapse(content, title, currNav);
+        } else {
+            collapse(content, title, currNav);
+        }
+
+    }
+
+    let superTokensPrevButtons = document.getElementsByClassName("docs-prev");
+    let superTokensNextButtons = document.getElementsByClassName("docs-next");
+    Array.from(superTokensPrevButtons).forEach(element => {
+        element.children[1].innerHTML = "Previous";
+    });
+
+    Array.from(superTokensNextButtons).forEach(element => {
+        element.children[0].innerHTML = "Next";
+    });
+
     let superTokensLinks = document.getElementsByTagName("a");
     Array.from(superTokensLinks).forEach(element => {
         let url = element.href;
@@ -12,45 +71,6 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 
-
-    function uncollapse(node, title, currNav) {
-        node.classList.remove("hide");
-        currNav.children[0].innerHTML = title + '<span class="arrow rotate"><svg width="24" height="24" viewBox="0 0 24 24"><path fill="#565656" d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6-6 6z"></path><path d="M0 0h24v24H0z" fill="none"></path></svg></span>';
-    }
-
-    function collapse(node, title, currNav) {
-        node.classList.add("hide");
-        currNav.children[0].innerHTML = title + '<span class="arrow"><svg width="24" height="24" viewBox="0 0 24 24"><path fill="#565656" d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6-6 6z"></path><path d="M0 0h24v24H0z" fill="none"></path></svg></span>'
-    }
-
-    let navGroupElements = document.getElementsByClassName("navGroup subNavGroup")
-
-    for (let i = 0; i < navGroupElements.length; i++) {
-        //   const element = array[i];
-        let currNav = navGroupElements[i];
-        const title = currNav.children[0].innerText;
-        const content = navGroupElements[i].childNodes[1];
-        currNav.children[0].classList.add("collapsible");
-        
-        
-        collapse(content, title, currNav);
-        currNav.childNodes[0].addEventListener("click", function () {
-            if (!content.classList.contains("hide")) {
-                collapse(content, title, currNav);
-            } else {
-                uncollapse(content, title, currNav);
-            }
-        });
- 
-    }
-
-    // let superTokensPrevButtons = document.getElementsByClassName("docs-prev");
-    // let superTokensNextButtons = document.getElementsByClassName("docs-next");
-    // Array.from(superTokensPrevButtons).forEach(element => {
-    //     element.children[1].innerHTML = "Previous";
-    // });
-
-    // Array.from(superTokensNextButtons).forEach(element => {
-    //     element.children[0].innerHTML = "Next";
-    // });
+    let body = document.getElementsByTagName("body")[0];
+    body.style.display = "block";
 });
