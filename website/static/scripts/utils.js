36f68fdc53831bc6700e37f0e5a671b2de294e93
Nemi Shah <nemishah1212@gmail.com>
2019-09-05 16:44:52 +0530

    Add latest version to feedback api

--- website/static/scripts/utils.js
+++ website/static/scripts/utils.js
@@ -1,3 +1,9 @@
+ let latestVersion = "4.3.X"; /*THIS COMMENT IS USED TO REPLACE THE LATEST VERSION*/
+
 function getGithubUrl() {
     return "https://github.com/supertokens/supertokens-node-mysql-ref-jwt";
+}
+
+function getLatestVersion() {
+    return latestVersion;
 }
