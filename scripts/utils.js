0bbc397d67bc0e82f4e1286dad3ce1647cc8c2ae
Rishabh Poddar <rishabh.poddar@gmail.com>
2020-03-14 06:07:33 +0000

    [skip ci]
    Deploy website version based on 3e6679ed887def18d44bd47f3ef06a2488ec6670

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,4 +1,4 @@
- let latestVersion = "5.0.1"; /*THIS COMMENT IS USED TO REPLACE THE LATEST VERSION*/
+ let latestVersion = "5.0.X"; /*THIS COMMENT IS USED TO REPLACE THE LATEST VERSION*/
 
 function getGithubUrl() {
     return "https://github.com/supertokens/supertokens-node-mysql-ref-jwt";
